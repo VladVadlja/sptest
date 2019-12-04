152cd5b9c2111a752dcd29f76fdfdc34dc58449b
Vladimir Vadlja <vladimir.vadlja@gmail.com>
2019-12-04 21:42:15 +0100

    Apply recomandations from rubocop

--- lib/file_not_found_error.rb
+++ lib/file_not_found_error.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Custom error when file is not found
 class FileNotFoundError < StandardError
-	def initialize(msg: 'File not found!')
-		super
-	end
+  def initialize(msg: 'File not found!')
+    super
+  end
 end
--- spec/unit/file_not_found_error_spec.rb
+++ spec/unit/file_not_found_error_spec.rb
@@ -1,23 +1,24 @@
+# frozen_string_literal: true
 
 require 'file_not_found_error'
 
 RSpec.describe FileNotFoundError do
-	subject { described_class.new }
+  subject { described_class.new }
 
-	describe 'initialization' do
-		it 'is standard error' do
-			expect(subject.class.superclass).to eq(StandardError)
-		end
+  describe 'initialization' do
+    it 'is standard error' do
+      expect(subject.class.superclass).to eq(StandardError)
+    end
 
-		it 'containes message' do
-			expect(subject.message).to eq({msg: "File not found!"}.to_s)
-		end
+    it 'containes message' do
+      expect(subject.message).to eq({ msg: 'File not found!' }.to_s)
+    end
 
-		context 'with custom message' do
-			it 'can be instantiated' do
-				other = described_class.new(msg: "Custom Message!")
-				expect(other.message).to eq({msg: "Custom Message!"}.to_s)
-			end
-		end
-	end
+    context 'with custom message' do
+      it 'can be instantiated' do
+        other = described_class.new(msg: 'Custom Message!')
+        expect(other.message).to eq({ msg: 'Custom Message!' }.to_s)
+      end
+    end
+  end
 end
